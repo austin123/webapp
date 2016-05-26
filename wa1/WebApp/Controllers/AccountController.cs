e8b4790b8dc19a7aa263105d7aaa283b431ef3e2
Austin <zam449@hotmail.com>
2016-05-26 14:58:22 -0700

    trigger re-test

--- wa1/WebApp/Controllers/AccountController.cs
+++ wa1/WebApp/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using WebApp.Models;
+//resting successful auto build
 
 namespace WebApp.Controllers
 {
